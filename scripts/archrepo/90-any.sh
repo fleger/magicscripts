bae30358a7bb632f4939ab2eebb15b61a01364fe
fleger <florian6.leger@laposte.net>
2011-03-02 18:21:51 +0100

    Fix archrepo

--- scripts/archrepo/90-any.sh
+++ scripts/archrepo/90-any.sh
@@ -8,5 +8,5 @@ mv "$1/$2" "$1/any/$2" &&
 ln -s "../any/$2" "$1/i686/$2" &&
 ln -s "../any/$2" "$1/x86_64/$2" &&
 repo-add "$1/i686/$REPO_DB" "$1/i686/$2" &&
-repo-add "$1/x86_64/$REPO_DB" "$1/x86_64/$2" &&
+repo-add "$1/x86_64/$REPO_DB" "$1/x86_64/$2" #&&
 # torssadd "$1/$REPO_RSS" """${BASH_REMATCH[1]} ${BASH_REMATCH[2]}-${BASH_REMATCH[3]} ${BASH_REMATCH[4]}" """$(tar -Oxaf "$1/${BASH_REMATCH[4]}/$2" .PKGINFO | grep "^pkgdesc =" | tail -c +11)""" "${BASE_URL}/${BASH_REMATCH[4]}/$2"
--- scripts/archrepo/90-x86_64.sh
+++ scripts/archrepo/90-x86_64.sh
@@ -3,5 +3,5 @@
 [[ "$2" =~ ^(.+)\-(.+)-([0-9]+)\-(x86_64)\.pkg\.tar\.[xg]z$ ]] &&
 mkdir -p "$1/x86_64" &&
 mv "$1/$2" "$1/x86_64/$2" &&
-repo-add "$1/x86_64/$REPO_DB" "$1/x86_64/$2" &&
+repo-add "$1/x86_64/$REPO_DB" "$1/x86_64/$2" #&&
 # torssadd "$1/$REPO_RSS" """${BASH_REMATCH[1]} ${BASH_REMATCH[2]}-${BASH_REMATCH[3]} ${BASH_REMATCH[4]}" """$(tar -Oxaf "$1/${BASH_REMATCH[4]}/$2" .PKGINFO | grep "^pkgdesc =" | tail -c +11)""" "${BASE_URL}/${BASH_REMATCH[4]}/$2"
