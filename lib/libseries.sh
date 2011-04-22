f4a908842a59002242623cd8a904434d10768210
fleger <florian6.leger@laposte.net>
2011-04-22 11:01:03 +0200

    libseries: ability to create mirror directories

--- lib/libseries.sh
+++ lib/libseries.sh
@@ -1,7 +1,12 @@
 . "$MAGICSCRIPTS_D/etc/series.conf"
 
 installEpisode() {
-  mkdir -p """$(dirname "$SERIES_BASE_PATH/$2")""" &&
+  local dirName="$(dirname "$2")"
+  mkdir -p "$SERIES_BASE_PATH/$dirName" &&
+  local d &&
+  for d in "${SERIES_MIRROR_DIRECTORIES[@]}"; do
+    mkdir -p "$d/$dirName"
+  done &&
   echo "Installing $1 to $SERIES_BASE_PATH/$2" &&
   mv -b "$1" "$SERIES_BASE_PATH/$2"
 }
