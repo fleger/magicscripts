838f85f2cef897edfd3cb0f627dfa1e954593eb9
fleger <florian6.leger@laposte.net>
2011-04-23 11:32:41 +0200

    libseries: install mirror directories before installing the real directory

--- lib/libseries.sh
+++ lib/libseries.sh
@@ -1,12 +1,12 @@
 . "$MAGICSCRIPTS_D/etc/series.conf"
 
 installEpisode() {
-  local dirName="$(dirname "$2")"
-  mkdir -p "$SERIES_BASE_PATH/$dirName" &&
+  local dirName="$(dirname "$2")" &&
   local d &&
   for d in "${SERIES_MIRROR_DIRECTORIES[@]}"; do
     mkdir -p "$d/$dirName"
   done &&
   echo "Installing $1 to $SERIES_BASE_PATH/$2" &&
+  mkdir -p "$SERIES_BASE_PATH/$dirName" &&
   mv -b "$1" "$SERIES_BASE_PATH/$2"
 }
