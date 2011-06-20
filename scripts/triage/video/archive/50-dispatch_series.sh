be3490bf486666292231303ebe6fe8dd179a09f2
fleger <florian6.leger@laposte.net>
2011-06-20 20:19:00 +0200

    Assorted fixes

--- scripts/triage/video/archive/50-dispatch_series.sh
+++ scripts/triage/video/archive/50-dispatch_series.sh
@@ -2,8 +2,13 @@
 
 local CORRECT_NAME_REGEX="^(.+)_([0-9]+)x([0-9]+)(.*)$"
 local filePath
+local series="$2"
+local season=""
 
 [ -f "$1/$2" ] &&
-[[ "$2" =~ $CORRECT_NAME_REGEX ]] &&
-filePath="${BASH_REMATCH[1]}/${BASH_REMATCH[1]}_Season_${BASH_REMATCH[2]}/$2" &&
+while [[ "$series" =~ $CORRECT_NAME_REGEX ]]; do
+  series="${BASH_REMATCH[1]}"
+  season="${BASH_REMATCH[2]}"
+done &&
+filePath="${series}/${series}_Season_${season}/${2}" &&
 installEpisode "$1/$2" "$filePath"
