949b944fcd3258cd25a91cf32f6df39856b26557
fleger <florian6.leger@laposte.net>
2011-06-20 20:22:00 +0200

    libseries: force relinking

--- lib/libseries.sh
+++ lib/libseries.sh
@@ -7,10 +7,10 @@ installEpisode() {
     mkdir -p "$d/$dirName"
   done &&
   echo "Installing $1 to $SERIES_BASE_PATH/$2" &&
-  mkdir -p "$SERIES_BASE_PATH/$dirName" &&
-  mv -b "$1" "$SERIES_BASE_PATH/$2"
+  mkdir -p "$SERIES_DESTINATION/$dirName" &&
+  mv -b "$1" "$SERIES_DESTINATION/$2"
   for d in "${SERIES_LINK_DIRECTORIES}"; do
     mkdir -p "$d"
-    ln -s "$SERIES_BASE_PATH/$2" "$d"
+    ln -sf "$SERIES_BASE_PATH/$2" "$d"
   done
 }
