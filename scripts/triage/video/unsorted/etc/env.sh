1290898a8931204742dba77a287cfb4b7ca76356
fleger <florian6.leger@laposte.net>
2011-07-01 20:06:39 +0200

    Fix stack behaviour

--- scripts/triage/video/unsorted/etc/env.sh
+++ scripts/triage/video/unsorted/etc/env.sh
@@ -5,3 +5,4 @@ export no_proxy=127.0.0.1,localhost,.local,.insa-rouen.fr
 export HTTP_PROXY=$http_proxy && export ftp_proxy=$http_proxy && export FTP_PROXY=$http_proxy && export https_proxy=$http_proxy && export HTTPS_PROXY=$http_proxy
 export NO_PROXY=$no_proxy
 export PATH=$HOME/bin:$PATH
+export LANG="fr_FR.UTF-8"
