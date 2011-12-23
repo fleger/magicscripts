5426d698bbb8cc817a023801f9066cb49161cc41
fleger <florian6.leger@laposte.net>
2011-12-23 09:55:54 +0100

    libseries: install dodona

--- lib/libseries.sh
+++ lib/libseries.sh
@@ -1,10 +1,22 @@
 . "$MAGICSCRIPTS_D/etc/series.conf"
 
+readonly DODONA_DIR="$HOME/projects/dodona/video"
+
+installDodona() {
+  [[ ! -f "$1/.dodona" ]] && install -Dm644 "$DODONA_DIR/dodona-season.sh" "$1/.dodona"
+  [[ ! -f "$1/.state" ]] && > "$1/.state"
+  local showDir=$(dirname "$1")
+  [[ ! -f "$showDir/.dodona" ]] && install -Dm644 "$DODONA_DIR/dodona-show.sh" "$showDir/.dodona"
+  [[ ! -f "$showDir/.state" ]] && > "$showDir/.state"
+  return 0
+}
+
 installEpisode() {
   local dirName="$(dirname "$2")" &&
   local d &&
   for d in "${SERIES_MIRROR_DIRECTORIES[@]}"; do
     mkdir -p "$d/$dirName"
+    installDodona "$d/$dirName"
   done &&
   echo "Installing $1 to $SERIES_BASE_PATH/$2" &&
   mkdir -p "$SERIES_DESTINATION/$dirName" &&
