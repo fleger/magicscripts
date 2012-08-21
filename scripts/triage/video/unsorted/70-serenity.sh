2da6ebdd90494b9b0be49de0a53b086ba309a43d
fleger <florian6.leger@laposte.net>
2012-08-21 07:16:45 +0200

    Use development version of serenity

--- scripts/triage/video/unsorted/70-serenity.sh
+++ scripts/triage/video/unsorted/70-serenity.sh
@@ -3,6 +3,6 @@
 [[ "$2" == *.@(avi|divx|mp4|mkv|flv|wmv|mpg|mpeg|webm|ogm|ogv|ass) ]] &&
 cd "$1" &&
 echo "Trying serenaming $1/$2..." &&
-VERSION_CONTROL="numbered" serenity -b "$1/$2" &&
+VERSION_CONTROL="numbered" "$HOME/projects/serenity/serenity-devel" rename -b "$1/$2" &&
 echo "Serenaming $1/$2 successful."
 
